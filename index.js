a270641f67112978abc68b0cd10acce280e59f5d
salam46khan <salam46.khan@gmail.com>
2025-04-10 14:56:35 +0600

    add coffee update and delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app= express()
 const port = process.env.PORT || 5000
 
@@ -40,7 +40,31 @@ async function run() {
         const result = await coffeeCollection.insertOne(coffee)
         res.send(result)
     })
-
+    app.delete('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result)
+    })
+    app.put('/coffee/:id', async(req, res)=>{
+      const id = req.params.id;
+      const coffee = req.body;
+      const query = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updateCoffee = {
+        $set: {
+          name: coffee.name,
+          chef: coffee.chef, 
+          supplier: coffee.supplier, 
+          taste: coffee.taste, 
+          category: coffee.category, 
+          details: coffee.details, 
+          photo: coffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(query, updateCoffee, options)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
